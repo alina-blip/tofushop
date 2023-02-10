cabd7206ffb6b3dc3b4287e3a863e25a67f4dfce
MalibuZ <122540439+MalibuZ@users.noreply.github.com>
2023-02-10 10:38:25 +0100

    nothing

--- components/navbar/loadNavbar.js
+++ components/navbar/loadNavbar.js
@@ -1,3 +1,3 @@
-$("#navbarContainer").load("/components/navbar/navbar.html");
-$("#container-nav-phone").load("/components/navbar/navbarPhone.html");
+$("#navbarContainer").load("./components/navbar/navbar.html");
+$("#container-nav-phone").load("./components/navbar/navbarPhone.html");
 
