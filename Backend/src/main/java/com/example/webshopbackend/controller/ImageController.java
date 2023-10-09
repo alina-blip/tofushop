fbd6a8d41c46ef4aeb0a4d8e01d0babacf883cde
Alina Sorger <alina.sorger@ebcont.com>
2023-10-09 10:25:53 +0200

    feat: delete notes

--- Backend/src/main/java/com/example/webshopbackend/controller/ImageController.java
+++ Backend/src/main/java/com/example/webshopbackend/controller/ImageController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
