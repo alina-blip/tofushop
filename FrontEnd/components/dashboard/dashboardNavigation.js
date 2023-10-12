97c8ec9468141b2914e2df5f08a1144839f51b7e
Alina Sorger <alina.sorger@ebcont.com>
2023-10-12 12:07:55 +0200

    feat: image upload and download in frontend

---
+++ FrontEnd/components/dashboard/dashboardNavigation.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    // Initial state:  "Registrierte Benutzer"
+    // $('#userList').show();
+    showUsers();
+    function showUsers() {
+        $('#userList').show();
+        $('#ordersList').hide();
+        $('#productsList').hide();
+        $('#addNewProduct').hide();
+        $('.cart-container h2').text('Registrierte Benutzer');
+    }
+    function showOrders() {
+        $('#userList').hide();
+        $('#ordersList').show();
+        $('#productsList').hide();
+        $('#addNewProduct').hide();
+        $('.cart-container h2').text('Bestellungen');
+
+    }
+    function showProducts() {
+        $('#userList').hide();
+        $('#ordersList').hide();
+        $('#productsList').show();
+        $('#addNewProduct').hide();
+        $('.cart-container h2').text('Produkte');
+
+    }
+    function showNewProductForm() {
+        $('#userList').hide();
+        $('#ordersList').hide();
+        $('#productsList').hide();
+        $('#addNewProduct').show();
+        $('.cart-container h2').text('Produkt hinzufügen');
+    }
+    $('#showUsersButton').click(showUsers);
+    $('#showOrdersButton').click(showOrders);
+    $('#showProductsButton').click(showProducts);
+    $('#showNewProductButton').click(showNewProductForm);
+
+});
