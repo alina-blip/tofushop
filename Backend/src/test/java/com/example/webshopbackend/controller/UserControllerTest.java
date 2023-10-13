5c6b393e7c5515e60e8ff68cf90baa25e1d3623c
Alina Sorger <alina.sorger@ebcont.com>
2023-10-13 13:34:41 +0200

    feat: test delete user

--- Backend/src/test/java/com/example/webshopbackend/controller/UserControllerTest.java
+++ Backend/src/test/java/com/example/webshopbackend/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import com.example.webshopbackend.dto.UserDTO;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -28,15 +30,19 @@ import java.util.List;
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
     @Test
+    @Order(2)
     void testAllUsersEndpoint() throws Exception {
+
+        System.out.println("THIS IS THE GET METHOD");
         mockMvc.perform(MockMvcRequestBuilders.get("/user")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andDo(MockMvcResultHandlers.print()); // This line prints the response
 
+    }
     @Test
+    @Order(1)
     void testSaveUserEndpoint() throws Exception {
         UserDTO userDTO = new UserDTO();
         userDTO.setId(1L);
@@ -49,6 +55,8 @@ public class UserControllerTest {
         userDTO.setStreet("Getreidegasse");
         userDTO.setPostalcode("1030");
 
+        System.out.println("THIS IS THE ID I POST IN THE DATABASE!!!" + userDTO.getId());
+
         mockMvc.perform(MockMvcRequestBuilders.post("/user")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(userDTO)))
@@ -57,22 +65,47 @@ public class UserControllerTest {
 
 
     @Test
+    @Order(3)
     void testDeleteUserEndpoint() throws Exception {
-        long userIdToDelete = 9;
+        // Create a user in the database
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        userDTO.setName("John");
+        userDTO.setSurname("Doe");
+        userDTO.setPassword("12345");
+        userDTO.setCountry("Graz");
+        userDTO.setEmail("John@Doe.com");
+        userDTO.setHousenumber("1A");
+        userDTO.setStreet("Getreidegasse");
+        userDTO.setPostalcode("1030");
 
+        // Save user to database
+        mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(userDTO)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        // delete the user
+        long userIdToDelete = 1;
+
+        String requestJson = "{\"id\": " + userIdToDelete + "}";
+
+        System.out.println(requestJson);
         String adminToken = generateAdminToken();
 
         mockMvc.perform(MockMvcRequestBuilders.delete("/user/" + userIdToDelete)
                         .header("Authorization", "Bearer " + adminToken)
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+
     private String generateAdminToken() {
         String secretKey = "thisismysecretkey";
-        int subject = 9;
+        int subject = 1;
         String email = "admin@example.com";
-        Instant expiration = Instant.now().plus(Duration.ofDays(1)); // Set a 1-day expiration
+        Instant expiration = Instant.now().plus(Duration.ofDays(1));
         List<String> roles = Collections.singletonList("ADMIN");
 
 
@@ -80,7 +113,7 @@ public class UserControllerTest {
                 .withSubject(String.valueOf(subject))
                 .withClaim("e", email)
                 .withClaim("a", roles)
-                .withExpiresAt(Date.from(expiration)) // Include the expiration claim
+                .withExpiresAt(Date.from(expiration))
                 .sign(Algorithm.HMAC256(secretKey));
 
         System.out.println("Generated Token: " + token);
