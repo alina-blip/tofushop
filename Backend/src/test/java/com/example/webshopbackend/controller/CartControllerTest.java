ffa863dfc27397372fdbca3b0a31d84ab519511c
Alina Sorger <alina.sorger@ebcont.com>
2023-10-18 14:04:41 +0200

    feat: testing and make cart work for POST and PUT

--- Backend/src/test/java/com/example/webshopbackend/controller/CartControllerTest.java
+++ Backend/src/test/java/com/example/webshopbackend/controller/CartControllerTest.java
@@ -1,26 +1,43 @@
 package com.example.webshopbackend.controller;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.example.webshopbackend.dto.CartDTO;
+import com.example.webshopbackend.service.CartService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CartControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Mock
+    private CartService cartService;
     @Test
     void testGetCartEndpoint() throws Exception {
-
         mockMvc.perform(MockMvcRequestBuilders.get("/cart")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andDo(MockMvcResultHandlers.print());
     }
+
+
+
+
 }
