e5cb04c43b7da27f3e8f03ac5922a1225c2729b2
alina-blip <118850621+alina-blip@users.noreply.github.com>
2023-10-19 15:33:00 +0200

    Update WebConfig.java

--- Backend/src/main/java/com/example/webshopbackend/config/WebConfig.java
+++ Backend/src/main/java/com/example/webshopbackend/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("")
-                .addResourceLocations("");
+     registry.addResourceHandler("/Frontend/images/upload-images/**")
+                .addResourceLocations("classpath:/static/img/");
     }
 }
